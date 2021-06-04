29714e07e503b793b5f24b5b60c8e8be8f42e8da
Saca Hernandez <rsacaher.ga@providence.edu>
2021-06-04 13:49:58 -0400

    cleared variables for other users

--- parameters.py
+++ parameters.py
@@ -1,13 +1,13 @@
 
 #search query
-search_query = 'site:linkedin.com/in/ AND "CFO" AND "United States"'
+search_query = 'site:linkedin.com/in/ AND "CFO" AND "United States"' #base query
 
 #file were results will be stored
 file_name = 'results.csv'
 
 #chromedriver path
-path = "C:/Users/rsacaher.ga/OneDrive - Providence College/Documents/chromedriver.exe"
+path = "" #input your own path
 
 #login credentials
-linkedin_password = ''
-linkedin_email = ''
+linkedin_password = '' #input your linkedin password
+linkedin_email = '' #input your linkedin email
